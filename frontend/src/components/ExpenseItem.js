6ecfe1e37fd7861f178963d6e2efb9f7610ea3b6
Faiz Basir <faiz.basir23@gmail.com>
2022-12-07 02:39:01 +0800

    updated transactions page

--- frontend/src/components/ExpenseItem.js
+++ frontend/src/components/ExpenseItem.js
@@ -1,7 +1,20 @@
 import React from "react";
+import Button from "../shared/UIElements/Button";
+import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
-  return <div></div>;
+const ExpenseItem = (props) => {
+  return (
+    <tr>
+      <td>{props.summary}</td>
+      <td>${props.amount}</td>
+      <td>{props.description}</td>
+      <td>{props.date}</td>
+      <div className="action">
+        <Button default>Edit</Button>
+        <Button danger>Delete</Button>
+      </div>
+    </tr>
+  );
 };
 
 export default ExpenseItem;
--- frontend/src/pages/Expenses.js
+++ frontend/src/pages/Expenses.js
@@ -1,12 +1,49 @@
 import React from "react";
-import "./Expenses.css";
+import { useParams } from "react-router-dom";
+import ExpenseList from "../components/ExpenseList";
+
+const DUMMY_EXPENSES1 = [];
+
+const DUMMY_EXPENSES = [
+  {
+    id: 1,
+    summary: "Petrol",
+    amount: 20,
+    description: "Pump petrol",
+    date: "18/10/2022",
+    user: "u1",
+  },
+  {
+    id: 2,
+    summary: "Food",
+    amount: 10,
+    description: "Macdonald's meal",
+    date: "18/10/2022",
+    user: "u1",
+  },
+  {
+    id: 3,
+    summary: "Airpods",
+    amount: 350,
+    description:
+      "Black Friday sale jsnfjsndafjdnfasnfdsjanf dnfsdafnsnna jdnajksndajks ndajsnd",
+    date: "18/10/2022",
+    user: "u2",
+  },
+  {
+    id: 4,
+    summary: "speaker",
+    amount: 350,
+    description: "Black Friday sale",
+    date: "18/10/2022",
+    user: "u2",
+  },
+];
 
 const Expenses = () => {
-  return (
-    <div className="center expense-list">
-      <h2>My Expenses here</h2>
-    </div>
-  );
+  const userId = useParams().userId;
+
+  return <ExpenseList items={DUMMY_EXPENSES} />;
 };
 
 export default Expenses;
