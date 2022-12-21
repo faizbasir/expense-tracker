1af9e2f7184e1d2f49188be15d6a8ce60837bdf1
Faiz Basir <faiz.basir23@gmail.com>
2022-12-21 23:40:47 +0800

    updated with context for login

--- frontend/src/pages/Expenses.js
+++ frontend/src/pages/Expenses.js
@@ -11,7 +11,7 @@ const DUMMY_EXPENSES = [
     amount: 20,
     description: "Pump petrol",
     date: "18/10/2022",
-    user: "u1",
+    user: "faizbasir",
   },
   {
     id: 2,
@@ -19,7 +19,7 @@ const DUMMY_EXPENSES = [
     amount: 10,
     description: "Macdonald's meal",
     date: "18/10/2022",
-    user: "u1",
+    user: "faizbasir",
   },
   {
     id: 3,
@@ -41,7 +41,9 @@ const DUMMY_EXPENSES = [
 ];
 
 const Expenses = () => {
-  const userId = useParams().userId;
+  const userId = useParams();
+
+  console.log(userId);
 
   return <ExpenseList items={DUMMY_EXPENSES} />;
 };
